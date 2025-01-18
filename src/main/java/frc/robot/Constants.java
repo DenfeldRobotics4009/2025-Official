f3943c06a5ed7a48b034beda8f20071a14406f13
EEEEEEEEEE1543 <kingofhollows099@gmail.com>
2025-01-18 13:28:36 -0600

    Finished Funnel Subsystem class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -99,4 +99,13 @@ public final class Constants {
   public static final class NeoMotorConstants {
     public static final double kFreeSpeedRpm = 5676;
   }
+
+  public static final class FunnelConstants{
+    public static final int agitatorMotorID = 0;
+    public static final int wideFunnelSensorChannel = 0;
+    public static final int shortFunnelSensorChannel = 1;
+    public static final int dropPistonChannel = 0;
+    public static final double laserSensorVoltageHigh = 0.58;
+    public static final double agitatorMotorSpeed = 0.5;
+  }
 }
--- src/main/java/frc/robot/subsystems/FunnelSubsystem.java
+++ src/main/java/frc/robot/subsystems/FunnelSubsystem.java
@@ -1,6 +1,41 @@
+package frc.robot.subsystems;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class FunnelSubsystem extends SubsystemBase{
     SparkMax agitatorMotor;
-    
+    AnalogInput wideFunnelSensor;
+    AnalogInput shortFunnelSensor;
+    Solenoid dropPiston;
+
+//Constructor
+    public FunnelSubsystem() {
+        this.agitatorMotor = new SparkMax(Constants.FunnelConstants.agitatorMotorID, null);
+        this.wideFunnelSensor = new AnalogInput(Constants.FunnelConstants.wideFunnelSensorChannel);
+        this.shortFunnelSensor = new AnalogInput(Constants.FunnelConstants.shortFunnelSensorChannel);
+        this.dropPiston =  new Solenoid(null, Constants.FunnelConstants.dropPistonChannel);
+    }
+//
+    public boolean getShortFunnelSensor() {
+        return shortFunnelSensor.getVoltage() < Constants.FunnelConstants.laserSensorVoltageHigh;
+    }
+    public boolean getWideFunnelSensor() {
+        return wideFunnelSensor.getVoltage() < Constants.FunnelConstants.laserSensorVoltageHigh;
+    }
+
+    //agitator on / off
+    public void agitatorMoterToggle(boolean agitatorOn){
+        if (agitatorOn == true) {
+            agitatorMotor.set(Constants.FunnelConstants.agitatorMotorSpeed);
+        } else {
+            agitatorMotor.set(0);
+        } 
+    }  
+    //Droppiston on / off
+    public void dropPiston(boolean dropPistonDown){
+            dropPiston.set(!dropPistonDown);
+    }
 } 
