3c5f3f8d0d14435442d3fbe7a19265438963fd08
rjincGD <ranjunkinc@gmail.com>
2025-01-20 12:10:57 -0600

    Added new SparkMax imports in Elevator.java - John

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.AbsoluteEncoder;
+
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
