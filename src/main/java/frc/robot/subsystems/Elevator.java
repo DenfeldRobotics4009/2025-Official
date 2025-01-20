95c513ed82edc6632a5472db6b8654471e1bfd01
EEEEEEEEEE1543 <trevorlee2008@gmail.com>
2025-01-20 11:21:50 -0600

    added a few doubles and some elevator functions -T

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -3,12 +3,16 @@ package frc.robot.subsystems;
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkMax;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
     SparkMax shaftMotorLeft;
     SparkMax shaftMotorRight;
     AbsoluteEncoder elevatorEncoder;
+
+    private PIDController movePIDController = new PIDController(0, 0, 0); //TODO: Add PID values in
+
     public enum setpoint{
         ZERO(0), //TODO: Add Constants in for the levels
         TRANSFER(1),
@@ -23,5 +27,16 @@ public class Elevator extends SubsystemBase {
         public double getEncoderValue(){
             return encoderValue;
         }
+    }
+    double offset;
+    public Elevator(){
+        //Resets encoder values
+        shaftMotorLeft.getEncoder().setPosition(0);
+        shaftMotorRight.getEncoder().setPosition(0);
+    }
+    @Override
+    public void periodic() {
+        // TODO Auto-generated method stub
+        super.periodic();
     }
 }
