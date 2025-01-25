4c0a364dd1872767ab7c3e2d194af18a5666ab6a
EEEEEEEEEE1543 <trevorlee2008@gmail.com>
2025-01-25 13:21:28 -0600

    Finished the Elevator subsystem and created a command for the subsystem

---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,62 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.ElevatorController;
+
+public class ElevatorSubsystem extends SubsystemBase {
+    private SparkMax shaftMotor;
+    public SparkMax getShaftMotor() {
+        return shaftMotor;
+    }
+    private DutyCycleEncoder elevatorEncoder;
+    public DutyCycleEncoder getElevatorEncoder() {
+        return elevatorEncoder;
+    }
+    private PIDController pid;
+    public PIDController getPid() {
+        return pid;
+    }
+    private double offset;
+
+    public ElevatorSubsystem(){
+        shaftMotor = new SparkMax(0, null); //TODO: get Spark IDs
+        elevatorEncoder = new DutyCycleEncoder(0); //TODO: Find actual channel
+        pid = new PIDController(offset, offset, offset);
+        setTarget(setpoint.ZERO);
+        setDefaultCommand(new ElevatorController(this));
+    }
+    
+    public void setTarget(setpoint var){
+        pid.setSetpoint(offset+var.encoderValue);
+    }
+
+    //Creates setpoints for the elevator to reach
+    public enum setpoint{
+        ZERO(0), //TODO: Add Constants in for the levels
+        P2(1),
+        P3(2),
+        P4(3);
+        double encoderValue;
+        setpoint(double val){
+            this.encoderValue = val;
+        }
+        public double getEncoderValue(){
+            return encoderValue;
+        }
+    }
+    public void setOffset(double newOffset){
+        this.offset = newOffset;
+    }
+    public void resetOffset(){
+        this.offset = 0;
+    }
+    @Override
+    public void periodic() {
+        // TODO Auto-generated method stub
+    }
+}
