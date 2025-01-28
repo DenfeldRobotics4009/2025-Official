5ed43e3a21103c16799b68e5514a716f41a7d49d
Tidas <kingofhollows099@gmail.com>
2025-01-28 17:03:04 -0600

    Deleted robot container import -Andrew

--- src/main/java/frc/robot/subsystems/FunnelSubsystem.java
+++ src/main/java/frc/robot/subsystems/FunnelSubsystem.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.RobotContainer;
 import frc.robot.commands.IntakeCommand;
 // "Tanner is stinky" -Luke
 public class FunnelSubsystem extends SubsystemBase{
