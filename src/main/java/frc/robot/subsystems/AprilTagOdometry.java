ab21ca56527e30f0a8d1f74cf66488edde82ed70
Trevor <trevorlee2008@gmail.com>
2025-02-04 18:41:09 -0600

    Stated creating functions for AprilTag detection and data interpretation.

--- src/main/java/frc/robot/subsystems/AprilTagOdometry.java
+++ src/main/java/frc/robot/subsystems/AprilTagOdometry.java
@@ -1,13 +1,52 @@
 package frc.robot.subsystems;
 
-import org.photonvision.PhotonCamera;
+import java.util.List;
 
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.targeting.TargetCorner;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AprilTagOdometry extends SubsystemBase{
     public PhotonCamera frontCam = new PhotonCamera("FrontCam");
     public PhotonCamera backCam = new PhotonCamera("BackCam");
     public AprilTagOdometry(){
+    }
 
+
+    public Pose2d seeAprilTags(){
+        PhotonPipelineResult readings = getCameraReadings();
+        PhotonPipelineResult relativeTOCamera = processReadings(readings);
+        Pose2d pos = getPosition(relativeToCamera);
+        return pos;
+    }
+
+    private Pose2d getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+
+    private void processReadings(PhotonPipelineResult result) {
+        boolean hasTargets = result.hasTargets();
+        List<PhotonTrackedTarget> targets = result.getTargets();
+        PhotonTrackedTarget target = result.getBestTarget();
+
+        double yaw = target.getYaw();
+        double pitch = target.getPitch();
+        double area = target.getArea();
+        double skew = target.getSkew();
+        Transform3d pose = target.getBestCameraToTarget();
+    }
+
+
+    private PhotonPipelineResult getCameraReadings(){
+        PhotonPipelineResult result = frontCam.getLatestResult();
+        return result;
     }
 }
