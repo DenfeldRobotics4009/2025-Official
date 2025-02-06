edc6313caeb9d77f1308addb62909b47b1691a03
Tidas <kingofhollows099@gmail.com>
2025-02-06 16:41:46 -0600

    Deleted the unused imports that were automatically made

--- src/main/java/frc/robot/autos/OnePieceCageAuto.java
+++ src/main/java/frc/robot/autos/OnePieceCageAuto.java
@@ -4,20 +4,15 @@ import java.io.IOException;
 
 import org.json.simple.parser.ParseException;
 
-import com.pathplanner.lib.util.FileVersionException;
+
 
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.library.auto.pathing.FollowControllers;
 import frc.library.auto.pathing.PurePursuitController;
 import frc.library.auto.pathing.PurePursuitSettings;
 import frc.library.auto.pathing.pathObjects.Path;
-import frc.robot.commands.FunnelDownCommand;
 import frc.robot.commands.IntakeCommand;
-import frc.robot.commands.SetElevatorTargetCommand;
-import frc.robot.subsystems.ElevatorSubsystem;
-import frc.robot.subsystems.FunnelSubsystem;
 import frc.robot.subsystems.ManipulatorSubsystem;
 import frc.robot.subsystems.SwerveDrive;
 
