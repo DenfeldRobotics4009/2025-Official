21dd45c213134036955e6c79a9cf56d233de9f55
Tidas <kingofhollows099@gmail.com>
2025-02-08 11:07:54 -0600

    Made manipulator sensor, when broken: start manipulator motor when not broken: stop manipulator motor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,7 +20,6 @@ import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.ClimberDownCommand;
 import frc.robot.commands.ClimberUpCommand;
-import frc.robot.commands.IntakeCommand;
 import frc.robot.commands.ManipulatorOutputCommand;
 import frc.robot.commands.SetElevatorTargetCommand;
 import frc.robot.commands.FunnelDownCommand;
@@ -110,9 +109,6 @@ public class RobotContainer {
         new Trigger(() -> {return m_controlsSubsystem.operateController.getRightTriggerAxis() >= 0.1;}).whileTrue(
         (new ManipulatorOutputCommand(m_manipulatorSubsystem))
         );
-
-        m_controlsSubsystem.getOperatePOVTrigger(180)
-        .whileTrue(new IntakeCommand(m_manipulatorSubsystem));
 
         new JoystickButton(m_controlsSubsystem.operateController, Button.kA.value)
         .onTrue(new SetElevatorTargetCommand(m_ElevatorSubsystem, setpoint.ZERO));
--- src/main/java/frc/robot/subsystems/FunnelSubsystem.java
+++ src/main/java/frc/robot/subsystems/FunnelSubsystem.java
@@ -1,42 +1,24 @@
 package frc.robot.subsystems;
 import com.revrobotics.spark.SparkMax;
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.commands.IntakeCommand;
 // "Tanner is stinky" -Luke
 public class FunnelSubsystem extends SubsystemBase{
-    //objects
-    
-    AnalogInput wideFunnelSensor;
+    //object
     SparkMax dropFunnelMotor; 
 
 //Constructor
     public FunnelSubsystem() {
-       
-        this.wideFunnelSensor = new AnalogInput(Constants.FunnelConstants.wideFunnelSensorChannel);
         this.dropFunnelMotor = new SparkMax(Constants.FunnelConstants.dropMotorChannel, null);
     }
     //tests for if object in lazers way
-   
-    public boolean getWideFunnelSensor() {
-        return wideFunnelSensor.getVoltage() < Constants.FunnelConstants.laserSensorVoltageHigh;
-        
-    }
     public void funnelDropMotorDown(){
 
     }
     public void funnelDropMotorUp(){
 
     }
-IntakeCommand runIntakeCommand = new IntakeCommand(ManipulatorSubsystem.getInstance());
-    @Override
-    public void periodic() {
-        if (getWideFunnelSensor() && !runIntakeCommand.isScheduled()) {
-            CommandScheduler.getInstance().schedule(runIntakeCommand);
-        }
-    }
+
 } 
 
 /* 
