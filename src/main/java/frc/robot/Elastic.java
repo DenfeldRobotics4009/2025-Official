a5dbc7a6e7d730cfb60a2475fcc619412e1e9fd9
Trevor <trevorlee2008@gmail.com>
2025-02-17 10:35:12 -0600

    Fixed minor issue

--- src/main/java/frc/robot/Elastic.java
+++ src/main/java/frc/robot/Elastic.java
@@ -1,10 +1,16 @@
 package frc.robot;
 
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.ManipulatorSubsystem;
 
-public class Elastic {
-    public void periodic(){
+
+public class Elastic extends SubsystemBase {
+
+    @Override
+    public void periodic() {
         SmartDashboard.putBoolean("Manipulator Motor Tripped", ManipulatorSubsystem.getInstance().getShortFunnelSensor());
     }
+  
 }
