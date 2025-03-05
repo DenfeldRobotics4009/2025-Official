1952ad594305d1bd48291312f185f532ef93912c
rjincGD <ranjunkinc@gmail.com>
2025-03-05 17:58:32 -0600

    Switching to apriltag branch

--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -95,7 +95,13 @@ public class SwerveDrive extends SubsystemBase implements DriveSubsystem {
 
     AutoBuilder.configure(
             this::getPosition,
-            this::zeroHeading
+            this::zeroHeading,
+            this::getModuleStates,
+            (speeds, feedforwards) -> drive(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+            new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
+                    new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                    new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+            );
 
     );
 
