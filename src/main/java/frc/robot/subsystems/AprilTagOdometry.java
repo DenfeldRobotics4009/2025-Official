867838595d632c9b890642528e4084834132e6ef
Bazinga947 <Andrewde2010@gmail.com>
2025-03-08 10:48:33 -0600

    Deleted printout

--- src/main/java/frc/robot/subsystems/AprilTagOdometry.java
+++ src/main/java/frc/robot/subsystems/AprilTagOdometry.java
@@ -66,7 +66,6 @@ public class AprilTagOdometry extends SubsystemBase{
             );
             System.out.println(positionSample.get().estimatedPose.toPose2d());
         }
-        System.out.println(frontCam.isConnected());
     }
     
     
