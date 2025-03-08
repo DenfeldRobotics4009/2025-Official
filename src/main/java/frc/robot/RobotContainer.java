f7a465e1e6b06c5e913f11c9164b05f6783face3
Tidas <kingofhollows099@gmail.com>
2025-03-08 11:49:48 -0600

    made test auto and added small thing in robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -63,6 +63,8 @@ import java.util.List;
 
 import org.json.simple.parser.ParseException;
 
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
 import frc.library.auto.pathing.PurePursuitController;
 import frc.library.auto.pathing.PurePursuitSettings;
 import frc.library.auto.pathing.field.FieldMirrorType;
@@ -268,6 +270,9 @@ public class RobotContainer {
     public Command getAutonomousCommand() {
         // An example command will be run in autonomous
         // return AutoShuffleboardTab.getInstance().getSelectedAuto();
-        return m_shuffleboard.getSelectedAuto();
+      //  return m_shuffleboard.getSelectedAuto();
+    
+        return new PathPlannerAuto("Example Auto");
+      }
     }
-}
+
