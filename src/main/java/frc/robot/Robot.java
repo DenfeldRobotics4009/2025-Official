c40712795db1231759dd0a5dabe52ca2997ced4b
rjincGD <ranjunkinc@gmail.com>
2025-03-11 21:41:19 -0500

    Added Red/Blue alliance detection to appropriately add gyro angle adjustment in AutoResetOdometry Moved AutoResetOdometry from autonomousInit to teleopInit in Robot.java Put current target apriltag yaw in shuffleboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.AutoResetOdometry;
+import frc.robot.commands.ResetSwerveOdometry;
 import frc.robot.subsystems.SwerveDrive;
 
 /**
@@ -101,8 +102,6 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
-  
-    new AutoResetOdometry();
   }
 
   /** This function is called periodically during autonomous. */
@@ -118,6 +117,8 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+    new ResetSwerveOdometry();
+    new AutoResetOdometry();
   }
 
   /** This function is called periodically during operator control. */
--- src/main/java/frc/robot/commands/AutoResetOdometry.java
+++ src/main/java/frc/robot/commands/AutoResetOdometry.java
@@ -1,20 +1,28 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.AprilTagOdometry;
 import frc.robot.subsystems.SwerveDrive;
 
   /**
-   * Only run this in autonomousInit.
+   * Only run this in teleopInit.
    */
 
 public class AutoResetOdometry extends Command {
 
     @Override
     public void initialize() {
-        double targetAprilTagYaw = AprilTagOdometry.getInstance().bestTarget().getYaw();
-        SwerveDrive.getInstance().m_gyro.setAngleAdjustment(targetAprilTagYaw - 180);
+        if (AprilTagOdometry.getInstance().bestTarget() != null) {
+            double targetAprilTagYaw = AprilTagOdometry.getInstance().bestTarget().getYaw();
+            if (DriverStation.Alliance.valueOf(getName()).equals(DriverStation.Alliance.Blue)) {
+                SwerveDrive.getInstance().m_gyro.setAngleAdjustment(targetAprilTagYaw - 180);
+            } else {
+                SwerveDrive.getInstance().m_gyro.setAngleAdjustment(targetAprilTagYaw);
+            }
+        }
     }
 
     @Override
