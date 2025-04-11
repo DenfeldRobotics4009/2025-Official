705bd45970a1bf33203774f57fcef72202c536e6
EEEEEEEEEE1543 <trevorlee2008@gmail.com>
2025-04-10 21:16:31 -0500

    Renamed back to algae manipulator (may still have some name errors because intellisense isn't working right)

--- src/main/java/frc/robot/commands/ToggleAlgaeManipulatorCommand.java
+++ src/main/java/frc/robot/commands/ToggleAlgaeManipulatorCommand.java
@@ -3,8 +3,8 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.L1CoralManipulatorSubsystem;
 
-public class ToggleL1CoralManipulatorCommand extends Command{
-private L1CoralManipulatorSubsystem m_l1CoralManipulatorSubsystem;
+public class ToggleAlgaeManipulatorCommand extends Command{
+private ToggleAlgaeManipulatorCommand m_l1CoralManipulatorSubsystem;
     public ToggleL1CoralManipulatorCommand(L1CoralManipulatorSubsystem algaeManipulator) {
     this.m_l1CoralManipulatorSubsystem = algaeManipulator;
     // Use addRequirements() here to declare subsystem dependencies.
