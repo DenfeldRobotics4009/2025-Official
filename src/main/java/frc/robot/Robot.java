87e93d3b156f086af24e74281b24cdc3aeeedea0
Bazinga947 <puppyisfluffy>
2025-05-10 15:40:48 -0500

    Branch for State

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -101,7 +101,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void disabledPeriodic() {
-    System.out.println(ElevatorSubsystem.getInstance().getElevatorRelativeEncoderValue());
+    
   }
 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
